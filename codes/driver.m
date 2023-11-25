4fd73b2b7b3071d592cb51c6cd4ef34cc6f7525e
togethe123lbh <1830744876@qq.com>
2023-11-25 19:08:47 +0800

    b-n_el=2

--- codes/driver.m
+++ codes/driver.m
@@ -2,25 +2,24 @@
 clear; clc;
 
 % =========================================================================
-% Problem definition
-% exact solution
-exact = @(x) x.^3;
+% Problem definition exact solution
 
+exact = @(x) x.^3;
+exact_dx=@(x) 3*x.^2;
 f = @(x) -6.0 * x;
 g = 1;
 h = 0;
 % =========================================================================
 
 % parameters of the FEM
-n_el  = 13;       % number of elements
+n_el  = 2;       % number of elements
 n_en  = 2;        % number of element nodes
 n_int = 3;        % number of quadrature points
-n_np  = n_el + 1; % number of points
+n_np  = n_el + 1; % number of points 
 n_eq  = n_np - 1; % number of equations
 
 % =========================================================================
-% Generate the mesh
-% nodal coordinates
+% Generate the mesh nodal coordinates
 hh     = 1 / n_el;
 x_coor = 0 : hh : 1;
 
@@ -49,7 +48,7 @@ F = zeros(n_eq, 1);    % allocate the global load vector
 % Assembly of K and F
 for ee = 1 : n_el
     k_e = zeros(n_en, n_en);
-    f_e = zeros(n_en, 1);
+    f_e = zeros(n_en, 1); 
     
     x_ele = x_coor(IEN(1:n_en,ee)); % A = IEN(a,e) and x_ele(a) = x_coor(A)
 
@@ -98,3 +97,32 @@ d_temp = K \ F;
 disp = [d_temp; g];
 
 % eof
+%2b
+el2up1=0.0;
+eH2up1=0.0;
+for ee=1:n_el
+    for ll=1:n_int
+        uh=0.0;
+        uh_dx=0.0;
+        xl=0.0;
+        dx_dxi=0.0;
+        for aa=1:n_en
+            uh=uh+disp(IEN(aa,ee))*PolyShape(aa,xi(ll),0);
+            uh_dx=uh_dx+disp(IEN(aa,ee))*PolyShape(aa,xi(ll),1);
+            xl=xl+x_coor(IEN(aa,ee))*PolyShape(aa,xi(ll),0);
+            dx_dxi=dx_dxi+x_coor(IEN(aa,ee))*PolyShape(aa,xi(ll),1);
+        end
+        el2up1=el2up1+weight(ll)*(uh-exact(xl)).^2*dx_dxi;
+        eH2up1=eH2up1+weight(ll)*(uh_dx-exact_dx(xl)).^2*dx_dxi;
+    end
+end
+el2up=el2up1.^(0.5);
+exact_2=@(x) x.^6;
+el2down=integral(exact_2,0,1).^(0.5);
+el2=el2up/el2down;
+eH2up=eH2up1.^(0.5);
+exact_dx2=@(x) 9*(x.^4);
+eH2down=integral(exact_dx2,0,1).^(0.5);
+eH2=eH2up/eH2down;
+ 
+
