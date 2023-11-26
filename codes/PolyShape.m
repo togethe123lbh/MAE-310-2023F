32e45de21894ee7e2435cff5316c307cafac27e7
togethe123lbh <1830744876@qq.com>
2023-11-26 11:18:29 +0800

    c-quadratic

--- codes/PolyShape.m
+++ codes/PolyShape.m
@@ -1,16 +1,69 @@
 function val = PolyShape(a, xi, der)
 
-if a == 1
+%linear element
+% if a == 1
+%     if der == 0
+%         val = 0.5 * (1-xi);
+%     elseif der == 1
+%         val = -0.5;
+%     end
+% elseif a == 2
+%     if der == 0
+%         val = 0.5 * (1+xi);
+%     elseif der == 1
+%         val = 0.5;
+%     else
+%     end
+% end
+
+% quadratic element
+if a==1
     if der == 0
-        val = 0.5 * (1-xi);
+        val=0.5*xi*(xi-1);
     elseif der == 1
-        val = -0.5;
+        val=-0.5+xi;
     end
 elseif a == 2
-    if der == 0
-        val = 0.5 * (1+xi);
-    elseif der == 1
-        val = 0.5;
-    else
+    if der==0
+        val=1-xi^2;
+    elseif der==1
+        val=-2*xi;
+    end
+elseif a == 3
+    if der==0
+        val=0.5*xi*(1+xi);
+    elseif der==1
+        val=0.5+xi;
     end
 end
+
+%cubic element
+% if a == 1
+%     if der == 0
+%         val = 0.5 * (1-xi);
+%     elseif der == 1
+%         val = -0.5;
+%     end
+% elseif a == 2
+%     if der == 0
+%         val = 0.5 * (1+xi);
+%     elseif der == 1
+%         val = 0.5;
+%     else
+%     end
+% elseif a == 3
+%     if der == 0
+%         val = 0.5 * (1-xi);
+%     elseif der == 1
+%         val = -0.5;
+%     end
+% elseif a == 4
+%     if der == 0
+%         val = 0.5 * (1+xi);
+%     elseif der == 1
+%         val = 0.5;
+%     else
+%     end
+% end
+
+
